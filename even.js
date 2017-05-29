a3fa5d64ecd838978f77e29847b021e627dcccae
Jonathan Lee <leejonathan87@gmail.com>
2017-05-29 00:57:03 +0000

    Adds functions exercises

---
+++ even.js
@@ -0,0 +1,9 @@
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+var tenIsEven = isEven(10123123123);
+var elevenIsEven = isEven(112038780237);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
