8ecdc9119aa490bd9807184580b69d0bc782878a
Jonathan Lee <leejonathan87@gmail.com>
2017-05-31 05:49:21 +0000

    Adds conditionals exercises

--- test2.js
+++
@@ -1,7 +0,0 @@
-// Do not modify these first two lines
-var iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
-console.log(iceCreamFlavours);
-
-// Your code below here...
-
-iceCreamFlavours.push("root beer");
